69b88565bd271f8f674ca722af1c9b005c6f6f96
akira-taniguchi <fesr82d5@yahoo.ne.jp>
2022-02-26 11:55:48 +0900

    first-commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $items = Author::all();
+        return view('index', ['items' => $items]);
+    }
+    public function add()
+    {
+        return view('add');
+    }
+    public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+    public function search(Request $request)
+    {
+        $item = Author::find($request->input);
+        dump($item);
+        $param = [
+            'item' => $item,
+            'input' => $request->input
+        ];
+        return view('find', $param);
+    }
+    public function bind(Author $author)
+    {
+        $data = [
+            'item' =>$author,
+        ];
+        return view('author.binds', $data);
+    }
+    public function create(Request $request)
+    {
+        $this->validate($request, Author::$rules);
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+    public function edit(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('edit', ['form' => $author]);
+    }
+    public function update(Request $request)
+    {
+        $this->validate($request, Author::$rules);
+        $form = $request->all();
+        unset($form['_token']);
+        Author::where('id', $request->id)->update($form);
+        return redirect('/');
+    }
+    public function delete(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('delete', ['form' => $author]);
+    }
+    public function remove(Request $request)
+    {
+        Author::find($request->id)->delete();
+        return redirect('/');
+    }
+    public function relate(Request $request) //追記
+    {
+        $hasItems = Author::has('book')->get();
+        $noItems = Author::doesntHave('book')->get();
+        $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
+        return view('author.index',$param);
+    }
+}
